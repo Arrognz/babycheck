4a5fbbb3b5e5586d66c507115f604a910a71f7d3
Arrognz <felix.ganz.94@gmail.com>
2025-12-29 17:38:47 +0100

    Fix run sh script

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 make
-go install -v .
+go build -o babycheck .
 go mod vendor
-heroku local --port 5001
+
+# Load environment variables if .env exists
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
+# Set default values if not set
+export PORT=${PORT:-5001}
+export GIN_MODE=${GIN_MODE:-debug}
+
+# Run the app
+./babycheck
